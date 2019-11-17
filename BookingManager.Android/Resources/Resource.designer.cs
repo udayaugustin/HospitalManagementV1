4c1c23a69655b305bfcea6b88d7038e12ac51ff5
udayaugustin <udayaugustin@gmail.com>
2019-11-17 13:03:15 +0530

    initial commit

--- BookingManager.Android/Resources/Resource.designer.cs
+++ BookingManager.Android/Resources/Resource.designer.cs
@@ -4442,13 +4442,10 @@ namespace BookingManager.Droid
 			public const int notify_panel_notification_icon_bg = 2131165293;
 			
 			// aapt resource value: 0x7F07006E
-			public const int sync = 2131165294;
+			public const int tooltip_frame_dark = 2131165294;
 			
 			// aapt resource value: 0x7F07006F
-			public const int tooltip_frame_dark = 2131165295;
-			
-			// aapt resource value: 0x7F070070
-			public const int tooltip_frame_light = 2131165296;
+			public const int tooltip_frame_light = 2131165295;
 			
 			static Drawable()
 			{
